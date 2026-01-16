cfa8177030386ea456de3d90ab6f241767f0911f
Raja Sai Datha Sushanth <rajasaidathasushanth2006@gmail.com>
2026-01-16 21:49:07 +0530

    f

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import Navbar from './Navbar';
+import Spinner from './Spinner';
 
 const Clients = () => {
 
   const [clients, setClients] = useState([])
+  const [loading, setLoading] = useState(true)
   const nav = useNavigate();
 
   const getClients = async () => {
     try {
+      setLoading(true)
       const response = await fetch('https://aarogyapath.onrender.com/admin/getclients')
       const res = await response.json()
       if (response.ok) {
@@ -16,6 +19,8 @@ const Clients = () => {
       }
     } catch (error) {
       console.log(error)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -29,43 +34,42 @@ const Clients = () => {
 
   return (
     <>
-    <Navbar />
-    <div className="w-full pl-[5%] h-full bg-gray-200 p-6">
-      <h1 className="text-2xl font-bold text-gray-800 mb-4">Clients</h1>
+      <Navbar />
+      <div className="w-full pl-[5%] h-full bg-gray-200 p-6">
+        <h1 className="text-2xl font-bold text-gray-800 mb-4">Clients</h1>
 
-      {sortedClients.length === 0 ? (
-        <p className="text-gray-700">No Clients</p>
-      ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 relative">
-          {sortedClients.map((client) => (
-            <div
-              key={client._id}
-              className={`
-                cursor-pointer bg-white rounded-xl shadow-lg p-5 transition-all duration-300
-                ${client.isvisited === false
-                  ? "z-50 bg-white scale-108 sm:scale-100 border-l-4 border-red-500"
-                  : "z-0 opacity-50 border-l-4 border-green-500"}
-              `}
-              onClick={()=>{nav(`/client/${client._id}`)}}
-            >
-              <div className="flex justify-between items-center mb-2">
-                <h2 className="text-lg font-semibold text-gray-800">{client.name}</h2>
-                <span className="text-sm px-2 py-1 bg-blue-100 text-blue-700 rounded-full">
-                  {client.gender}
-                </span>
+        {loading ? <Spinner />: sortedClients.length === 0 ? (
+          <p className="text-gray-700">No Clients</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 relative">
+            {sortedClients.map((client) => (
+              <div
+                key={client._id}
+                className={`
+                  cursor-pointer bg-white rounded-xl shadow-lg p-5 transition-all duration-300
+                  ${client.isvisited === false
+                    ? "z-50 bg-white scale-108 sm:scale-100 border-l-4 border-red-500"
+                    : "z-0 opacity-50 border-l-4 border-green-500"}
+                `}
+                onClick={() => { nav(`/client/${client._id}`) }}
+              >
+                <div className="flex justify-between items-center mb-2">
+                  <h2 className="text-lg font-semibold text-gray-800">{client.name}</h2>
+                  <span className="text-sm px-2 py-1 bg-blue-100 text-blue-700 rounded-full">
+                    {client.gender}
+                  </span>
+                </div>
+
+                <p><b>Profession:</b> {client.proffession}</p>
+                <p><b>Height:</b> {client.height}</p>
+                <p><b>Weight:</b> {client.weight}</p>
+
               </div>
-
-              <p><b>Profession:</b> {client.proffession}</p>
-              <p><b>Height:</b> {client.height}</p>
-              <p><b>Weight:</b> {client.weight}</p>
-
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+            ))}
+          </div>
+        )}
+      </div>
     </>
-    
   )
 }
 
